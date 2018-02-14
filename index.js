5b3aa325e5d4e8b19a0a67cc9e96d8baa6d377d1
Nathan Pownell <dirato2000@gmail.com>
2018-02-14 15:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+const kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(kittens) {
+  const moreKittens = [...kittens, "Ralph"]
+  return moreKittens
+}
+function destructivelyPrependKitten(kittens) {
+  return ["Bob", ...kittens]
+}
+function destructivelyRemoveLastKitten(kittens) {
+  
+}
+function destructivelyRemoveFirstKitten(kittens) {
+  
+}
+function appendKitten(kittens) {
+  const moreKittens  
+}
+function prependKitten(kittens) {
+  
+}
+function removeLastKitten(kittens) {
+  
+}
+function removeFirstKitten() {
+  
+}
